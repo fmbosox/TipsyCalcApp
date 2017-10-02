def5f51be6219cb5aef0a5bd8e8ed205d2e0bf3c
Felipe Montoya <felipemontoya@akwa.local>
2017-10-01 23:12:28 -0700

    First commit of the Tipsy Calc App

---
+++ TipsyCalcApp/Controller/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  TipsyCalcApp
+//
+//  Created by Felipe Montoya on 10/1/17.
+//  Copyright © 2017 Felipe Montoya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    
+    @IBOutlet weak var tipPercentageSlider: UISlider!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    var tip = tipModel(billAmount: 0.0, tipPercent: 0.0)
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTipCalculationValues()
+        updateUI()
+    }
+    
+    func setTipCalculationValues () {
+        tip.tipPercent = Double(tipPercentageSlider.value)
+        tip.billAmount = ((textField.text)! as NSString).doubleValue
+        tip.calculateTip()
+    }
+    
+    func updateUI () {
+        tipLabel.text = String (format: "$%0.2f", tip.tipAmount)
+        totalLabel.text = String (format: "$%0.2f", tip.totalAmount)
+        tipPercentageLabel.text = "Tip: \(Int(tipPercentageSlider.value * 100))%"
+    }
+
+    
+    @IBAction func billAmountDidChange(_ sender: Any) {
+        print("It changed")
+        setTipCalculationValues()
+        updateUI()
+    }
+    @IBAction func tipPercentageDidChange(_ sender: UISlider) {
+        let steps: Float = 100
+        let roundedValue = round(sender.value * steps) / steps
+        sender.value = roundedValue
+        print(tipPercentageSlider.value)
+        setTipCalculationValues()
+        updateUI()
+    }
+}
+
